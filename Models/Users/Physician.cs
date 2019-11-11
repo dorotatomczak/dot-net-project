3facdcb2fa79de74185e751d45b12546771c9c19
unknown <a.malizjusz@vp.pl>
2019-11-11 22:25:35 +0100

    Implemented multilanguage option. Add some models that might be useful

---
+++ Models/Users/Physician.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClinic.Models
+{
+    enum PhysicianSpecialization
+    {
+        Internist,
+        Laryngologist,
+        Radiologist,
+        Pulmonologist,
+        Surgeon,
+        Cardiologist,
+        Allergist,
+        Psychologist,
+        Psychiatrist
+    }
+
+    public class Physician : Person
+    {
+        PhysicianSpecialization specialization { get; set; }
+        //working hours?
+    }
+}
---
+++ Models/Users/Receptionist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClinic.Models
+{
+    public class Receptionist : Person
+    {
+    }
+}
