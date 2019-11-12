3767629932c23b0cfccaa52c397e46543bd36c69
unknown <a.malizjusz@vp.pl>
2019-11-12 22:18:17 +0100

    Add menu for physician, receptionist and patient

--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,13 @@
-using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using WebClinic.Data;
@@ -72,7 +65,8 @@ namespace WebClinic
                 options.SupportedUICultures = supportedCultures;
                 options.RequestCultureProviders = new List<IRequestCultureProvider>
                 {
-                    new CookieRequestCultureProvider()
+                    new CookieRequestCultureProvider(), // use cookie if exists
+                    new AcceptLanguageHeaderRequestCultureProvider() // = use web browser settings otherwise
                 };
             });
         }
