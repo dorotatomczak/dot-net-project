84aca25cc679541409d4a4c03afa0dbd3fc5d277
Ada <adapiekarska97@gmail.com>
2019-11-16 17:13:43 +0100

    Add backend api for appointment, patient and physician

--- Models/Users/Patient.cs
+++ Models/Users/Patient.cs
@@ -1,8 +1,13 @@
-﻿namespace WebClinic.Models.Users
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WebClinic.Models.Users
 {
     public class Patient : AppUser
     {
         public string IllnessHistory { get; set; }
         public string RecommendedDrugs { get; set; }
+        [JsonIgnore]
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
