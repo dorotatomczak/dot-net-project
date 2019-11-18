31bb252dd90cb62ed07e7b0558888fe15fb1511d
Dorota <tomczakd65@gmail.com>
2019-11-18 18:48:03 +0100

    Add translations, error messages, redirect after login

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WebClinic.Models
 {
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "The '{0}' field is required.")]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The '{0}' field is required.")]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
