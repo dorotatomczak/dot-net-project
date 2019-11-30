43d318f08bac87f1091bf0e5fe05a3706ebe37bf
unknown <a.malizjusz@vp.pl>
2019-11-30 13:43:25 +0100

    Make list of patients work. Update namespaces. Not done: show errors when API call throws exception

--- WebClinicGUI/Models/Users/AppUser.cs
+++ WebClinicGUI/Models/Users/AppUser.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebClinicGUI.Models.Users
 {
     public enum Sex
     {
+        [Display(Name = "Male")]
         Male,
+        [Display(Name = "Female")]
         Female,
+        [Display(Name = "It's complicated")]
         ItsComplicated
     }
 
@@ -16,6 +20,7 @@ namespace WebClinicGUI.Models.Users
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
         public Sex Sex { get; set; }
         public string Role { get; set; }
