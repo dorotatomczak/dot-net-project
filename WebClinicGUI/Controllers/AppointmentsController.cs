2ec26d87749926885cc22b5686c9b25b83b335ba
Ada <adapiekarska97@gmail.com>
2019-12-05 18:52:45 +0100

    Add success alert after successful appointment canceling

--- WebClinicGUI/Controllers/AppointmentsController.cs
+++ WebClinicGUI/Controllers/AppointmentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +16,12 @@ namespace WebClinicGUI.Controllers
     [Authorize(Roles = "Receptionist")]
     public class AppointmentsController : Controller
     {
+        private readonly IStringLocalizer<AddAppointmentController> _localizer;
         private readonly INetworkClient _client;
 
-        public AppointmentsController(INetworkClient client)
+        public AppointmentsController(IStringLocalizer<AddAppointmentController> localizer, INetworkClient client)
         {
+            _localizer = localizer;
             _client = client;
         }
 
@@ -28,6 +31,7 @@ namespace WebClinicGUI.Controllers
             try
             {
                 var appointment = await _client.SendRequestAsync<Appointment>(HttpMethod.Delete, $"Appointments/{id}");
+                TempData["Message"] = "AppointmentCanceled";
                 return RedirectToAction(nameof(CalendarController.Receptionist), "Calendar");
             }
             catch (HttpRequestException)
